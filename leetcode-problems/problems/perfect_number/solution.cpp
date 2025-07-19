1a7606aa9ef51f9ada37ae16bb930e25af0adefd
ramreddy-tadi <tadiramreddy.7@gmail.com>
2025-07-19 06:08:06 +0000

    Sync LeetCode submission - Perfect Number (cpp)

---
+++ leetcode-problems/problems/perfect_number/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum = 0;
+        for(int i=1;i*i<=num;i++)
+        {
+            if(num%i==0)
+            {
+                sum+=i;
+                if(i!=num/i)
+                {
+                    sum+=num/i;
+                }
+            }
+        }
+        sum-=num;
+        return (sum==num)?true:false;
+    }
+};
