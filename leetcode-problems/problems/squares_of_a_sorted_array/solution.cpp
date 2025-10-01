1afc7cdbdfa5cec24c3c73580d01266a79a7f737
ramreddy-tadi <tadiramreddy.7@gmail.com>
2025-10-01 15:04:08 +0000

    Sync LeetCode submission - Squares of a Sorted Array (cpp)

---
+++ leetcode-problems/problems/squares_of_a_sorted_array/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int>res;
+        for(int i:nums)
+        {
+            res.push_back(abs(i*i));
+        }
+        sort(res.begin(),res.end());
+        return res;
+    }
+};
