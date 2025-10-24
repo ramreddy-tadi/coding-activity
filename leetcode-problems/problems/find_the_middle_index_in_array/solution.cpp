d6dd3d16f013601062ef5e2bde99ad76341b72cc
ramreddy-tadi <tadiramreddy.7@gmail.com>
2025-10-24 09:16:06 +0000

    Sync LeetCode submission - Find the Middle Index in Array (cpp)

---
+++ leetcode-problems/problems/find_the_middle_index_in_array/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+         int sum =0;
+        for(int ch:nums)
+        {
+            sum+=ch;
+        }
+        int ls=0;
+        int rs=sum;
+        for(int i=0;i<nums.size();i++)
+        {
+            int rs= rs-nums[i];
+            if(ls==rs)
+            {
+                return i;
+            }
+            ls=ls+nums[i];
+        }
+        return -1;
+    }
+};
