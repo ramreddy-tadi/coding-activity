ea9da847643393ca46b702f0cc04e5eb1b669fa4
ramreddy-tadi <tadiramreddy.7@gmail.com>
2025-12-07 17:43:29 +0000

    Sync LeetCode submission - Sort an Array (cpp)

--- leetcode-problems/problems/sort_an_array/solution.cpp
+++ leetcode-problems/problems/sort_an_array/solution.cpp
@@ -1,7 +1,55 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        mergeSort(nums,0,nums.size()-1);
         return nums;
     }
+    void merge(vector<int>&arr,int l,int r,int mid)
+    {
+        int i=l;
+        int j=mid+1;
+        int k=0;
+        int n = r-l+1;
+        int temp[n];
+        while(i<=mid&&j<=r)
+        {
+            if(arr[i]<=arr[j])
+            {
+                temp[k]=arr[i];
+                i++;
+                k++;
+            }
+            else{
+                temp[k]=arr[j];
+                j++;
+                k++;
+            }
+        }
+        while(i<=mid)
+        {
+            temp[k]=arr[i];
+            i++;
+            k++;
+        }
+        while(j<=r)
+        {
+            temp[k]=arr[j];
+            j++;
+            k++;
+        }
+        for(int c=0;c<n;c++)
+        {
+            arr[l+c]=temp[c];
+        }
+    }
+void mergeSort(vector<int>&arr, int l, int r) {
+        if(l<r)
+        {
+            int mid = l+(r-l)/2;
+            mergeSort(arr,l,mid);
+            mergeSort(arr,mid+1,r);
+            merge(arr,l,r,mid);
+        }
+    }
+
 };
